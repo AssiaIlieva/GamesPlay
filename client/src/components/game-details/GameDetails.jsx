b071d48d20af9da08172a824c2048699fc4bd6d3
AssiaIlieva <ac17@abv.bg>
2024-07-20 14:38:23 +0300

    Show dynamic gameDetails

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -1,56 +1,67 @@
-export default function Component() {
-    return (
-        <section id="game-details">
-            <h1>Game Details</h1>
-            <div className="info-section">
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-                <div className="game-header">
-                    <img className="game-img" src="images/MineCraft.png" />
-                    <h1>Bright</h1>
-                    <span className="levels">MaxLevel: 4</span>
-                    <p className="type">Action, Crime, Fantasy</p>
-                </div>
+import gamesAPI from '../../api/games-api';
 
-                <p className="text">
-                    Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                    with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                    creatures live side by side with humans. A human cop is forced
-                    to work with an Orc to find a weapon everyone is prepared to kill for.
-                </p>
+export default function GameDetails() {
+  const [game, setGame] = useState({});
+  const { gameId } = useParams();
 
-                {/* <!-- Bonus ( for Guests and Users ) --> */}
-                <div className="details-comments">
-                    <h2>Comments:</h2>
-                    <ul>
-                        {/* <!-- list all comments for current game (If any) --> */}
-                        <li className="comment">
-                            <p>Content: I rate this one quite highly.</p>
-                        </li>
-                        <li className="comment">
-                            <p>Content: The best game.</p>
-                        </li>
-                    </ul>
-                    {/* <!-- Display paragraph: If there are no games in the database --> */}
-                    <p className="no-comment">No comments.</p>
-                </div>
+  useEffect(() => {
+    (async () => {
+      const result = await gamesAPI.getOne(gameId);
+      setGame(result);
+    })();
+  }, []);
 
-                {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
-                <div className="buttons">
-                    <a href="#" className="button">Edit</a>
-                    <a href="#" className="button">Delete</a>
-                </div>
-            </div>
+  return (
+    <section id="game-details">
+      <h1>Game Details</h1>
+      <div className="info-section">
+        <div className="game-header">
+          <img className="game-img" src={game.imageUrl} />
+          <h1>{game.title}</h1>
+          <span className="levels">MaxLevel: {game.maxLevel}</span>
+          <p className="type">{game.category}</p>
+        </div>
+        <p className="text">{game.summary}</p>
 
-            {/* <!-- Bonus -->
+        {/* <!-- Bonus ( for Guests and Users ) --> */}
+        <div className="details-comments">
+          <h2>Comments:</h2>
+          <ul>
+            {/* <!-- list all comments for current game (If any) --> */}
+            <li className="comment">
+              <p>Content: I rate this one quite highly.</p>
+            </li>
+            <li className="comment">
+              <p>Content: The best game.</p>
+            </li>
+          </ul>
+          {/* <!-- Display paragraph: If there are no games in the database --> */}
+          <p className="no-comment">No comments.</p>
+        </div>
+
+        {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
+        <div className="buttons">
+          <a href="#" className="button">
+            Edit
+          </a>
+          <a href="#" className="button">
+            Delete
+          </a>
+        </div>
+      </div>
+
+      {/* <!-- Bonus -->
             <!-- Add Comment ( Only for logged-in users, which is not creators of the current game ) --> */}
-            <article className="create-comment">
-                <label>Add new comment:</label>
-                <form className="form">
-                    <textarea name="comment" placeholder="Comment......"></textarea>
-                    <input className="btn submit" type="submit" value="Add Comment" />
-                </form>
-            </article>
-
-        </section>
-    );
+      <article className="create-comment">
+        <label>Add new comment:</label>
+        <form className="form">
+          <textarea name="comment" placeholder="Comment......"></textarea>
+          <input className="btn submit" type="submit" value="Add Comment" />
+        </form>
+      </article>
+    </section>
+  );
 }
