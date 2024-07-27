9eb389bdd50eebddfd2f7dec0c12f9fe5c509c48
AssiaIlieva <ac17@abv.bg>
2024-07-27 19:12:59 +0300

    Update Header - add dynamic navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,23 +1,35 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../../contexts/AuthContext';
+
 export default function Header() {
-    return (
-        <header>
-            {/* <!-- Navigation --> */}
-            <h1><Link className="home" to="/">GamesPlay</Link></h1>
-            <nav>
-                <Link to="/games">All games</Link>
-                {/* <!-- Logged-in users --> */}
-                <div id="user">
-                    <Link to="/game/create">Create Game</Link>
-                    <Link to="#">Logout</Link>
-                </div>
-                {/* <!-- Guest users --> */}
-                <div id="guest">
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                </div>
-            </nav>
-        </header>
-    );
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <header>
+      {/* <!-- Navigation --> */}
+      <h1>
+        <Link className="home" to="/">
+          GamesPlay
+        </Link>
+      </h1>
+      <nav>
+        <Link to="/games">All games</Link>
+        {isAuthenticated ? (
+          <div id="user">
+            <Link to="/game/create">Create Game</Link>
+            <Link to="#">Logout</Link>
+          </div>
+        ) : (
+          <div id="guest">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
+        {/* <!-- Logged-in users --> */}
+
+        {/* <!-- Guest users --> */}
+      </nav>
+    </header>
+  );
 }
